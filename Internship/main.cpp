2704f27ab85544b9c32f03e5d682902f1a1ed901
Nida-Akhtar <nidapervaiz17@gmail.com>
2023-09-07 23:27:36 +0500

    Changed the main of code. Wrote the code for a number guessing game.

--- Internship/main.cpp
+++ Internship/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  Internship
-//
-//  Created by Nida on 9/7/23.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <cstdlib> // For random number generation
+#include <ctime>   // For seeding the random number generator
+using namespace std;
+int main() {
+    
+  srand(static_cast<unsigned int>(time(nullptr)));
+  
+    int randomNumber = rand() % 100 + 1;
+    int userGuess;
+    int numberOfGuesses = 0;
+   cout << "                    Welcome!    \n";
+   cout << "------------ Number Guessing Game ---------------      \n";
+   
+   
+   cout << "Rule or Guess: The number is between 1 and 100. Try to guess it.\n";
+    do {
+        cout << "Enter your guess: ";
+        cin >> userGuess;
+        numberOfGuesses++;
+        if (userGuess > randomNumber)
+        {
+        cout << "Too high! Try again.\n";
+        }
+        else if (userGuess < randomNumber)
+        {
+        cout << "Too low! Try again.\n";
+        }
+        else {
+        cout << "Congratulations! You guessed the number in " << numberOfGuesses << " guesses.\n";
+        }
+    }
+    while (userGuess != randomNumber);
     return 0;
 }
+
+
+
