5f0dbb88109ebd6886af6abeb48b3d85083823a8
Nida-Akhtar <nidapervaiz17@gmail.com>
2023-09-07 23:58:33 +0500

    Code for simple calculator

--- Task2 (Simple Calculator)/main.cpp
+++ Task2 (Simple Calculator)/main.cpp
@@ -1,114 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
-struct Task {
-    string description;
-    bool completed;
-
-    Task(const string& desc) : description(desc), completed(false) {}
-};
-
-
-void addTask(vector<Task>& tasks, const string& description) {
-    Task task(description);
-    tasks.push_back(task);
-   cout << "Task added: " << description << endl;
-}
-
-
-void viewTasks(const std::vector<Task>& tasks) {
-    if (tasks.empty()) {
-        cout << "No tasks in the list." << endl;
-    } else {
-       cout << "Task List:" << endl;
-        for (size_t i = 0; i < tasks.size(); ++i) {
-          cout << i + 1 << ". ";
-            if (tasks[i].completed) {
-              cout << "[X] ";
-            } else {
-                cout << "[ ] ";
-            }
-            cout << tasks[i].description << std::endl;
-        }
-    }
-}
-
-
-void markTaskCompleted(vector<Task>& tasks, int taskNumber) {
-    if (taskNumber >= 1 && taskNumber <= tasks.size()) {
-        tasks[taskNumber - 1].completed = true;
-       cout << "Task marked as completed: " << tasks[taskNumber - 1].description << endl;
-    } else {
-        cout << "Invalid task number." << endl;
-    }
-}
-
-
-void removeTask(vector<Task>& tasks, int taskNumber) {
-    if (taskNumber >= 1 && taskNumber <= tasks.size()) {
-       cout << "Task removed: " << tasks[taskNumber - 1].description << endl;
-        tasks.erase(tasks.begin() + taskNumber - 1);
-    } else {
-        cout << "Invalid task number." <<endl;
-    }
-}
-
-int main() {
-   vector<Task> tasks;
-
-    while (true) {
-        cout << "\nOptions:\n";
-        cout << "1. Add Task\n";
-       cout << "2. View Tasks\n";
-      cout << "3. Mark Task as Completed\n";
-        cout << "4. Remove Task\n";
-        cout << "5. Exit\n";
-       cout << "Enter your choice: ";
-
-        int choice;
-        cin >> choice;
-
-        if (choice == 5) {
+int main()
+{
+    double num1, num2;
+    char op;
+    cout << "----------Simple Calculator-------------\n";
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+    double result;
+    switch (op)
+    {
+        case '+':
+            result = num1 + num2;
             break;
-        }
-
-        switch (choice) {
-            case 1: {
-                cin.ignore();
-            string description;
-               cout << "Enter task description: ";
-               getline(cin, description);
-                addTask(tasks, description);
-                break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+            } else {
+                cout << "Error: Division by zero!\n";
+                return 1;
             }
-            case 2: {
-                viewTasks(tasks);
-                break;
-            }
-            case 3: {
-                viewTasks(tasks);
-                int taskNumber;
-                cout << "Enter the task number to mark as completed: ";
-                cin >> taskNumber;
-                markTaskCompleted(tasks, taskNumber);
-                break;
-            }
-            case 4: {
-                viewTasks(tasks);
-                int taskNumber;
-               cout << "Enter the task number to remove: ";
-                cin >> taskNumber;
-                removeTask(tasks, taskNumber);
-                break;
-            }
-            default:
-               cout << "Invalid choice. Please try again." << endl;
-                break;
-        }
+            break;
+        default:
+           cout << "Invalid operator!\n";
+            return 1;
     }
-
+  cout << "Result: " << result << endl;
     return 0;
 }
+
+
 
